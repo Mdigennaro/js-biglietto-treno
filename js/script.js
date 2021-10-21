8f547cc798084a8183248846a2342c2a1ac2f5b0
Mdigennaro <michele.digennaro719@gmail.com>
2021-10-21 18:51:59 +0200

    fine

--- js/script.js
+++ js/script.js
@@ -7,31 +7,45 @@
 */
 
 const distance = prompt (`Inserisci chilometri da percorrere`);
-
 const age = prompt (`Inserisci età`);
 
-let price = distance * 0.21
+let discount18 = 20;
+let discount65 = 40;
 
-if(age < 18){
-  console.log(`100 * price / 20`)
-}
+let price = distance * 0.21;
 
 let bigliettoValido = true;
-let errorNum = ""
+let errorNum = "";
+
+if(isNaN(distance)){
+  bigliettoValido = false;
+  errorNum = `Inserire una  distanza valida`;
+}  
+
+if(isNaN(age)){
+  bigliettoValido = false;
+  errorNum = `Inserire un età valida`;
+}  
+
+console.log('bigliettoValido',bigliettoValido)
+
+// fai i modo che qua ci entri solo se il biglietto è valido perchè altirmenti è inutile
+if(age < 18){
+  discount18 = Math.round((price * discount18) / 100);
+  price =  price - discount18;
+}else if(age > 65){
+  discount65 = Math.round((price * discount65) / 100);
+  price = price - discount65;
+}
 
 console.log(distance);
 console.log(age);
 console.log(price);
 
-if(isNaN(distance)){
-  bigliettoValido = false
-  errorNum = `Inserire una  distanza valida`
-}
+document.getElementById(`output`).innerHTML =
+`
+Distanza: ${distance} <br>
+Età: ${age} <br>
+Prezzo: ${price} € <br>
 
-if(isNaN(age)){
-  bigliettoValido = false
-  errorNum = `Inserire un età valida`
-}
-
-console.log('bigliettoValido',bigliettoValido)
-console.log('errornNum',errorNum)
+`
