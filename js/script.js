828958a0938db6ef7965098a309e2103f7ac6d63
Mdigennaro <michele.digennaro719@gmail.com>
2021-10-21 16:35:45 +0200

    Calcolo prezzo

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*
+
+1. Chiedi all'utente il numero di chilometri da percorrere
+2. Chiedi l'età
+3. Calcolare il prezzo del viaggio con <18 con il 20% di sconto e >65 con 40% di sconto, il prezzo viene 0.21€
+
+*/
+
+const distance = prompt (`Inserisci chilometri da percorrere`);
+
+const age = prompt (`Inserisci età`);
+
+let price = distance * 0.21
+
+console.log(distance);
+console.log(age);
+console.log(price);
+
